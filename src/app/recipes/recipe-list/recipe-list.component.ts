4b7fe93f63b7acff73be8c2bac698f2aed058715
imdivyansh710 <imdivyansh710@gmail.com>
2025-03-04 22:59:48 +0530

    Updated recipe list and recipe detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe("Test recipe",
+       "this is a sample test recipe", 
+       "https://www.indianhealthyrecipes.com/wp-content/uploads/2023/09/egg-toast-recipe.jpg"),
+       new Recipe("Test recipe",
+        "this is a sample test recipe", 
+        "https://www.indianhealthyrecipes.com/wp-content/uploads/2023/09/egg-toast-recipe.jpg")
+  ];
 }

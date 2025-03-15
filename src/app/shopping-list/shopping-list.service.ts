9366d48771c7d61f76105d39e4d66fc2ba1fdaaa
imdivyansh710 <imdivyansh710@gmail.com>
2025-03-15 22:48:39 +0530

    added subject inplace of event emitter

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,10 +1,10 @@
-import { EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
+import { Subject } from "rxjs";
 
 export class ShoppingListService {
 
    private ingredients: Ingredient[] = [];
-   ingredientChanged = new EventEmitter<Ingredient[]>();
+   ingredientChanged = new Subject<Ingredient[]>();
 
     getIngredients() {
         return this.ingredients.slice();
@@ -12,11 +12,11 @@ export class ShoppingListService {
 
     onIngredientAdded(ingredient: Ingredient) {
 		this.ingredients.push(ingredient);
-        this.ingredientChanged.emit(this.ingredients.slice());
+        this.ingredientChanged.next(this.ingredients.slice());
 	}
 
     addIngredients(ingredients: Ingredient[]) {
         this.ingredients.push(...ingredients);
-        this.ingredientChanged.emit(this.ingredients.slice());
+        this.ingredientChanged.next(this.ingredients.slice());
     }
 }

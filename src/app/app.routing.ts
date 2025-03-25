b845c3791772c964c0264a14db6c455d072b4f4b
imdivyansh710 <imdivyansh710@gmail.com>
2025-03-25 18:16:08 +0530

    adding auth component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,6 +6,7 @@ import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.com
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipeResolverService } from "./recipes/recipes-resolver.service";
+import { AuthComponent } from "./auth/auth.component";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipe', pathMatch: 'full' },
@@ -15,7 +16,8 @@ const appRoutes: Routes = [
         {path: ':id', component: RecipeDetailComponent, resolve: [RecipeResolverService]},
         {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipeResolverService] },
     ] },
-    { path: 'shopping-list', component: ShoppingListComponent }];
+    { path: 'shopping-list', component: ShoppingListComponent },
+    { path: 'auth', component: AuthComponent }];
 
 @NgModule({
     imports: [RouterModule.forRoot(appRoutes)],

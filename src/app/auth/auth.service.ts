5395619fab422628437ffb4edaa2356b71a2639d
imdivyansh710 <imdivyansh710@gmail.com>
2025-03-25 20:19:55 +0530

    added error handling for login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, throwError } from "rxjs";
 
@@ -24,27 +24,47 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }).pipe(catchError(
-                (errorRes) => {
-                    let errorMessage = "An unknown error occured!"
-                    if (!errorRes.error || !errorRes.error.error) {
-                        return throwError(errorMessage)
-                    }
-                    switch (errorRes?.error?.error?.message) {
-                        case 'EMAIL_EXISTS':
-                            errorMessage = 'This Email exists already!'
-                    }
-                    return throwError(errorMessage);
-                } ))
+                this.handleError
+            ))
     }
 
     login(email: string, password: string) {
-      return this.http.post<AuthResponse>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCsDNKSP-LlX0fDwfJleWTuIk9rxbE-ARw',
-        {
-            email: email,
-            password: password,
-            returnSecureToken: true
+        return this.http.post<AuthResponse>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCsDNKSP-LlX0fDwfJleWTuIk9rxbE-ARw',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }).pipe(catchError(
+                this.handleError
+            ))
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = "An unknown error occured!"
+        console.log(errorRes);
+        
+        if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage)
         }
-      )
+        switch (errorRes?.error?.error?.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'This Email exists already!'
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'This Email was not found!'
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'This password is not correct!'
+                break;
+            case 'INVALID_LOGIN_CREDENTIALS':
+                errorMessage = 'Login Credentials are not correct!'
+                break;
+            case 'USER_DISABLED':
+                errorMessage = 'The user account has been disabled!'
+                break;
+        }
+
+        return throwError(errorMessage);
     }
 
 }

bcff5642a93431d93424307b4259e336eb6ffc20
imdivyansh710 <imdivyansh710@gmail.com>
2025-03-31 16:31:35 +0530

    added dialog for error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptor } from './auth/auth.interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import { AuthInterceptor } from './auth/auth.interceptor.service';
     LoadingSpinnerComponent,
     AuthComponent,
     HeaderComponent,
+    AlertComponent,
     DropdownDirective
   ],
   imports: [
